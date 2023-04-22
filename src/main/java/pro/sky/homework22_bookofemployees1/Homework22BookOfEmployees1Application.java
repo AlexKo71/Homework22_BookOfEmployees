977a744cf77b242ac0609c6bad080f6b9010a6db
Alexey Kolodin <alekko-sp@yandex.ru>
2023-04-22 18:03:59 +0300

    there is an empty branch here

---
+++ src/main/java/pro/sky/homework22_bookofemployees1/Homework22BookOfEmployees1Application.java
@@ -0,0 +1,13 @@
+package pro.sky.homework22_bookofemployees1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework22BookOfEmployees1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework22BookOfEmployees1Application.class, args);
+    }
+
+}
